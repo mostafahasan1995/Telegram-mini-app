d359718ca5b2e08639c57326c70361058410a79f
MOSTAFA94HASAN <stevhasan17@gmail.com>
2025-03-19 23:06:31 +0200

    Integration With Telegram Mini App

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <h4>Telegram Mini App2</h4>;
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.Telegram?.WebApp) {
+      const tg = window.Telegram.WebApp;
+      tg.expand(); 
+
+      const user = tg.initDataUnsafe?.user;
+      if (user?.username) {
+        setUsername(user.username);
+        console.log("تم الاتصال بـ Telegram WebApp ✅");
+        console.log("اسم المستخدم:", user.username);
+      } else {
+        console.warn("⚠️ لم يتم العثور على اسم المستخدم!");
+      }
+    } else {
+      console.error("❌ لم يتم تحميل Telegram WebApp API");
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome To Telegram Mini App</h1>
+      {username ? <p> User Name{username}</p> : <p>Loading ...</p>}
+    </div>
+  );
 }
